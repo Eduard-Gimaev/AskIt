2d7f697d4654eae1b682fbe83ef94395c62174f2
GED <gimaev.ufa@gmail.com>
2024-10-11 00:31:23 +0200

    feat: added flash notice

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -15,8 +15,10 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
     if @question.save
+      flash[:notice] = 'Question was successfully created.'
       redirect_to questions_path
     else
+      flash.now[:alert] = 'There was an error creating the question.'
       render :new
     end
   end
@@ -25,22 +27,32 @@ class QuestionsController < ApplicationController
   end
 
   def update
-    @question.update(question_params) ? redirect_to(questions_path) : render(:edit)
+    if @question.update(question_params)
+      flash[:notice] = 'Question was successfully updated.'
+      redirect_to questions_path
+    else
+      flash.now[:alert] = 'There was an error updating the question.'
+      render :edit
+    end
   end
 
   def destroy
     if @question.destroy
+      flash[:notice] = 'Question was successfully deleted.'
+      redirect_to questions_path
+    else
+      flash[:alert] = 'There was an error deleting the question.'
       redirect_to questions_path
     end
   end
 
   private
 
-  def question_params
-    params.require(:question).permit(:title, :body)
-  end 
-
   def set_question
     @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :body)
   end
 end
