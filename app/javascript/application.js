2350956ca638e6f366172cf61126064211c7b0b8
GED <gimaev.ufa@gmail.com>
2024-10-24 13:14:08 +0200

    feat: added tags and search for them

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,11 +3,9 @@ import "controllers"
 import * as bootstrap from "bootstrap"
 import "bootstrap-icons/font/bootstrap-icons.css"
 import "stylesheets/application"
-import "jquery"
-import "./select2_init"
-
-
-// Инициализация всех элементов с классом .collapse
+import "./tom_select_init"; // Correct import for tom_select_init.js in the same directory
+console.log("Application.js is loaded");
+// Initialize all elements with the .collapse class
 document.addEventListener("turbo:load", () => {
   document.querySelectorAll('.collapse').forEach((collapseElement) => {
     new bootstrap.Collapse(collapseElement)
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.0/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.6/dist/esm/index.js"
-pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js", preload: true
-pin "select2", to: "https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-rc.0/js/select2.min.js"
+pin "tom-select", to: "https://cdn.jsdelivr.net/npm/tom-select@2.0.0/dist/js/tom-select.complete.min.js"
